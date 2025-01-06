0717a0ba43d6ac07d36f1eb7b075bf2b2fcdd28a
Survivor-L <959324023@qq.com>
2025-01-06 20:56:10 +0800

    新增对数组的练习

--- test.c
+++ test.c
@@ -849,3 +849,24 @@
 //	//vs2022不支持变长数组
 //	return;
 //}
+#include <stdio.h>
+int main() {
+	int x = 3;
+	int y = 3;
+	printf("%d", x % 2);
+	switch (x % 2) {
+	case 1:
+		switch (y)
+		{
+		case 0:
+			printf("first");
+		case 1:
+			printf("second");
+			break;
+		default: printf("hello");
+		}
+	case 2:
+		printf("third");
+	}
+	return 0;
+}
