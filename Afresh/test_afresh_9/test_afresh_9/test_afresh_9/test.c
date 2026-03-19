6cee88edf3be934a74ec12dd77a58073a56bc4dc
Survivor-L <959324023@qq.com>
2026-03-19 20:58:50 +0800

    1

---
+++ Afresh/test_afresh_9/test_afresh_9/test_afresh_9/test.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int Add(int x,int y) 
+{
+	return x + y;
+}
+int main()
+{
+	int a = 1;
+	int b = 2;
+	int c =	Add(a,b);
+	printf("%d", c);
+	return 0;
+}
