d39c24a4c981a7bb9fce4955177894b53c8f80bf
Survivor-L <959324023@qq.com>
2026-04-05 21:31:37 +0800

    结构体详解

--- Afresh/test_afresh_20/afresh_20/afresh_20/test.c
+++ Afresh/test_afresh_20/afresh_20/afresh_20/test.c
@@ -1,6 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
-#include <stdio.h>
+//#include <stdio.h>
 //int main()
 //{
 //	/*KiKi学习了循环，BoBo老师给他出了一系列打印图案的练习，该任务是打印用“* ”组成的X形图案。
@@ -36,22 +36,77 @@
 //输出描述：
 //针对每行输入，输出用“ * ”组成的“空心”正方形，每个“ * ”后面有一个空格。
 
+//int main()
+//{
+//	int input = 0;
+//	int i = 0;
+//	int j = 0;
+//	while(scanf("%d", &input)!=EOF)
+//	for ( i = 0; i < input; i++)
+//	{
+//		for ( j = 0; j < input; j++)
+//		{
+//			if (i == 0 || i == input - 1 || j == 0 || j == input - 1)
+//				printf("* ");
+//			else
+//				printf("  ");
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+
+#include <stdio.h>
+#include <string.h>
+//结构体  结构体的定义
+//结构体用于将不同类型的数据组合成一个新的数据类型
+//struct 结构体名 {
+//    数据类型 成员1;
+//    数据类型 成员2;
+//    // ...
+//};
+struct Student {
+    int id;
+    char name[20];
+    float score;
+};
+//struct Student {
+//    int id;
+//    char name[20];
+//} stu2, stu3; //  方式二：定义类型的同时声明变量
+
+//省略类型名（匿名结构体
+//struct {
+//    int id;
+//    char name[20];
+//} stu1;
+
+
 int main()
 {
-	int input = 0;
-	int i = 0;
-	int j = 0;
-	while(scanf("%d", &input)!=EOF)
-	for ( i = 0; i < input; i++)
-	{
-		for ( j = 0; j < input; j++)
-		{
-			if (i == 0 || i == input - 1 || j == 0 || j == input - 1)
-				printf("* ");
-			else
-				printf("  ");
-		}
-		printf("\n");
-	}
+    //结构体变量声明
+    //方式一：先定义类型，后声明变量
+    //struct Student stu1;
+    
+   // 按顺序初始化（C89 / C99）：
+        struct Student stu1 = { 1001, "张三", 95.5 };
+   // 指定成员初始化（C99）：
+        struct Student stu2 = { .id = 1001, .score = 95.5, .name = "张三" };
+
+        //赋值后逐个成员初始化
+        struct Student stu;
+        stu.id = 1001;
+        strcpy(stu.name, "张三");
+        stu.score = 95.5;
+
+        //访问结构体成员
+            //使用点运算符（.） 直接访问
+            printf("%d\n", stu.id);
+
+        //使用箭头运算符（->） 通过指针访问
+            struct Student* p = &stu;
+            p->id = 1002;   // 等价于 (*p).id = 1002;
+            printf("%f", p->score);
 	return 0;
 }
